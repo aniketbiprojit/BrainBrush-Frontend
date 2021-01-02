7e1f88ef840503b60d4e81e0b3b07a3c55d788e0
Abhinav Robinson <abhinavrobinson@gmail.com>
2021-01-02 16:21:29 +0530

    (feat): add search in nav

--- components/Navbar/Navbar.tsx
+++ components/Navbar/Navbar.tsx
@@ -14,6 +14,18 @@ const Navbar = () => (
 					<a className='text-white font-semibold no-underline'>Brainbrush</a>
 				</Link>
 			</li>
+			<div className='relative flex w-full sm:w-7/12 md:w-5/12 px-4 flex-wrap items-stretch lg:ml-auto'>
+				<div className='flex'>
+					<span className='font-normal leading-snug flex text-center white-space-no-wrap border border-solid border-black rounded-full text-sm bg-white items-center rounded-r-none pl-2 py-1 text-black border-r-0 placeholder-primary'>
+						<i className='fas fa-search'></i>
+					</span>
+				</div>
+				<input
+					type='text'
+					className='px-2 py-1 h-8 border border-solid  border-black rounded-full text-sm leading-snug text-black bg-white shadow-none outline-none focus:outline-none w-full font-normal rounded-l-none flex-1 border-l-0 placeholder-primary'
+					placeholder='Search for forcasts, reports ...'
+				/>
+			</div>
 			<ul className='flex items-center justify-between space-x-5'>
 				{links.map(({ href, label }) => (
 					<li key={`${href}${label}`}>
