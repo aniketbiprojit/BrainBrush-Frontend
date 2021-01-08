3e32be60bab6bc60d12a7e3505a7f3aeea71ac1a
Abhinav Robinson <abhinavrobinson@gmail.com>
2021-01-08 18:48:37 +0530

    (feat): extract styles in search

--- components/modules/Search/Search.tsx
+++ components/modules/Search/Search.tsx
@@ -1,17 +1,23 @@
 import SearchWithButton from '../SearchWithButton/SearchWithButton'
 
 const Search = (props) => {
+	const styles = {
+		container: 'flex flex-col items-center w-screen pt-10',
+		search: 'grid w-screen px-20 justify-items-center',
+		recent: 'mt-10',
+	}
+
 	return (
-		<div className='flex flex-col items-center w-screen pt-10'>
-			<div className='grid w-screen px-20 justify-items-center'>
+		<div className={styles.container}>
+			<div className={styles.search}>
 				<SearchWithButton
 					msg='Get up to date with the latest happenings!'
 					placeholder='Search for listing, financial reports, companies ...'
 					button='Search'
 				/>
-				<div id='recent' className='mt-5'>
-					*Recent searches here*
-				</div>
+			</div>
+			<div id='recent' className={styles.recent}>
+				*Recent searches here*
 			</div>
 		</div>
 	)
