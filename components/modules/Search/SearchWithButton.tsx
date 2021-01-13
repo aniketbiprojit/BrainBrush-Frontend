eaa1c7f34c0b3e6cb0dd7d5cc29209bdf1bb068b
Abhinav Robinson <abhinavrobinson@gmail.com>
2021-01-13 19:33:00 +0530

    (feat): enlarge search

--- components/modules/Search/SearchWithButton.tsx
+++ components/modules/Search/SearchWithButton.tsx
@@ -12,13 +12,13 @@ const SearchWithButton = (props) => {
 		box: 'grid text-center gap-3 w-full',
 		container: 'justify-self-center relative w-full md:w-7/12 lg:w-5/12 w-3/12 flex-wrap items-stretch flex text-center',
 		text:
-			'px-4 py-6 h-10 border border-solid border-black rounded-md text-sm leading-snug text-black bg-white shadow-none outline-none focus:outline-none w-full font-normal flex-1 placeholder-primary border-r-0 rounded-r-none',
-		button: 'px-4 py-3 border-solid border-black rounded-md text-white bg-secondary-light outline-none red-button rounded-l-none',
+			'px-4 py-10 h-10 border border-solid border-black rounded-xl text-2xl leading-snug text-black bg-white shadow-none outline-none focus:outline-none w-full font-normal flex-1 placeholder-primary border-r-0 rounded-r-none',
+		button: 'px-4 py-6 border-solid border-black rounded-xl text-white text-2xl bg-secondary-light outline-none red-button rounded-l-none',
 		head: 'text-primary text-center text-lg md:text-3xl font-bold pb-4',
 	}
 
 	if (props.color === 'black') {
-		styles.head = 'text-gray-900 text-center text-lg md:text-3xl font-bold pb-4'
+		styles.head = 'text-gray-900 text-center text-xl md:text-4xl font-bold pb-4'
 	}
 
 	return (
