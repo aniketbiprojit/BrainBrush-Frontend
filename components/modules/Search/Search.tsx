7dad880e321271f890e1a49dd743a3a5a348d251
Abhinav Robinson <abhinavrobinson@gmail.com>
2021-01-14 16:04:20 +0530

    (feat): fix offaxis svg banner

--- components/modules/Search/Search.tsx
+++ components/modules/Search/Search.tsx
@@ -20,7 +20,7 @@ const Search = (props) => {
 			<div id='recent' className={styles.recent}>
 				*Recent searches here*
 			</div>
-			<svg className='relative w-screen mb-' viewBox='0 0 3108 650' fill='none'>
+			<svg className='relative w-screen transform translate-y-2' viewBox='0 0 3108 650' fill='none'>
 				<path
 					d='M554 193.5C386.5 86 135 19 0.5 0.5V1159H3108V480.12C2948.57 344.348 2661.49 288.314 2266.5 391C1953 472.5 1850 537 1434 537C1018 537 725.25 303.407 554 193.5Z'
 					fill='rgb(229, 231, 235)'
