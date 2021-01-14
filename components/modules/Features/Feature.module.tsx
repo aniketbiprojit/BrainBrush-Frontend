37e6fd60579dc4b67fd1303cf69da8b055355a0e
Abhinav Robinson <abhinavrobinson@gmail.com>
2021-01-14 14:34:05 +0530

    (feat): rm import

--- components/modules/Features/Feature.module.tsx
+++ components/modules/Features/Feature.module.tsx
@@ -1,5 +1,5 @@
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faArrowCircleDown } from '@fortawesome/free-solid-svg-icons'
+// import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+// import { faArrowCircleDown } from '@fortawesome/free-solid-svg-icons'
 
 const Feature = (props) => {
 	let image
