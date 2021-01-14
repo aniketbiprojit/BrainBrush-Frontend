a10d44387d231e5513b9c7eeabbcbd1b178ed116
Abhinav Robinson <abhinavrobinson@gmail.com>
2021-01-14 15:22:09 +0530

    (feat): responsive hover

--- components/modules/Features/Features.tsx
+++ components/modules/Features/Features.tsx
@@ -9,7 +9,7 @@ const Features = () => {
 			<div className='grid text-6xl text-white justify-items-center grid-col-1 '>
 				<FontAwesomeIcon
 					icon={faArrowDown}
-					className='p-2 duration-500 transform -translate-y-56 bg-transparent rounded-xl ring-4 ring-secondary-highlight hover:-translate-y-48 hover:bg-secondary-highlight hover:text-black'
+					className='p-2 duration-500 transform -translate-y-32 bg-transparent lg:-translate-y-56 rounded-xl ring-4 ring-secondary-highlight hover:-translate-y-24 lg:hover:-translate-y-48 hover:bg-secondary-highlight hover:text-black'
 					onClick={() => document.getElementById('features').scrollIntoView({ behavior: 'smooth', block: 'start', inline: 'nearest' })}
 				/>
 			</div>
