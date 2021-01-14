9baa538a3fb80759b42bca2639092cd7294e2d4b
Abhinav Robinson <abhinavrobinson@gmail.com>
2021-01-14 16:02:40 +0530

    (feat): complete responsive pass

--- components/modules/Blogs/Blogs.tsx
+++ components/modules/Blogs/Blogs.tsx
@@ -3,7 +3,7 @@ import Blog from './Blog.module'
 const Blogs = () => {
 	return (
 		<div className='flex flex-col justify-center w-full gap-20 lg:gap-32'>
-			<div className='text-5xl font-semibold text-center text-primary header'>Here are some relevant stories for you!</div>
+			<div className='px-5 text-5xl font-semibold text-center text-primary header lg:px-0'>Here are some relevant stories for you!</div>
 			<div className='flex flex-col m-auto lg:flex-row blogs'>
 				<Blog
 					companyName='Microsoft'
