d494ea7fd567cfc33184ee914bf39b9805479bb6
Abhinav Robinson <abhinavrobinson@gmail.com>
2021-01-16 15:02:30 +0530

    (feat): add new items to footer

--- components/modules/Footer/Footer.tsx
+++ components/modules/Footer/Footer.tsx
@@ -9,13 +9,13 @@ const Footer = (props) => {
 	]
 
 	const styles = {
-		container: 'bg-primary flex flex-col lg:flex-row items-center justify-between p-20',
+		container: 'bg-primary flex flex-col lg:flex-row items-center justify-between p-14',
 		search: 'justify-items-end w-full p-0 contents-end items-end',
 		menu: {
 			container: 'flex items-center justify-between space-x-5',
 			text: 'text-white no-underline hover:text-secondary-light',
 		},
-		copy: 'text-white no-underline text-center w-full',
+		copy: 'text-white no-underline text-center w-full pt-16 border-t border-gray-500',
 	}
 
 	return (
@@ -30,6 +30,11 @@ const Footer = (props) => {
 						</li>
 					))}
 				</ul>
+				<div className='flex items-center justify-between px-10 py-5 text-white lg:py-0'>
+					<div className='mx-2 text-center'>Contact Us</div>
+					<div className='mx-2 text-center'>Privacy Policy</div>
+					<div className='mx-2 text-center'>Our Terms & Conditions</div>
+				</div>
 			</div>
 			<div className={styles.container}>
 				<div className={styles.copy}>© BrainBrush 2020. All rights reserved.</div>
