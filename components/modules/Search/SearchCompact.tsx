506c21655ee95a9eb82a703d7fae9619d76d42eb
Abhinav Robinson <abhinavrobinson@gmail.com>
2021-01-19 20:15:55 +0530

    (feat): better scrollbar

--- components/modules/Search/SearchCompact.tsx
+++ components/modules/Search/SearchCompact.tsx
@@ -12,7 +12,7 @@ const SearchBar = (props) => {
 	const styles = {
 		container: 'relative w-full sm:w-7/12 lg:w-5/12 px-4 flex-wrap items-stretch hidden',
 		icon:
-			'font-normal leading-snug flex text-center white-space-no-wrap border border-solid border-black rounded-md text-sm bg-primary-highlight items-center rounded-l-none pr-3 pl-3 py-2 text-black border-l-0 items-center placeholder-primary',
+			'font-normal leading-snug flex text-center white-space-no-wrap border border-solid border-black rounded-md text-sm bg-primary-highlight items-center rounded-l-none pr-3 pl-3 py-2 text-black items-center placeholder-primary',
 		text:
 			'px-3 py-3 h-10 border border-solid border-black rounded-md text-sm leading-snug text-black bg-white shadow-none outline-none focus:outline-none w-full font-normal rounded-r-none flex-1 border-r-0 placeholder-primary',
 	}
