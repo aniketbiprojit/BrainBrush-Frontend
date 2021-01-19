9108b3ee1205d55504530199c2535db4543cceca
Abhinav Robinson <abhinavrobinson@gmail.com>
2021-01-19 20:26:31 +0530

    (feat): msg fix

--- components/modules/Reviews/Review.module.tsx
+++ components/modules/Reviews/Review.module.tsx
@@ -1,11 +1,13 @@
 const Review = (props) => {
 	return (
 		<div
-			className='grid grid-cols-2 grid-rows-5 gap-5 p-10 mb-10 text-center duration-300 bg-white shadow-2xl lg:transform hover:-translate-y-8 lg:mb-0 align-items-center rounded-2xl'
+			className='grid grid-cols-2 grid-rows-5 gap-5 p-10 mx-5 mb-10 text-center duration-300 bg-white shadow-2xl w-400px lg:transform hover:-translate-y-8 lg:mb-0 align-items-center rounded-2xl'
 			id='review-card'
 		>
 			<div className='text-4xl font-semibold text-gray-800 justify-self-start'>{props.companyName}</div>
-			<div className='col-span-2 row-span-4 m-auto text-xl text-justify text-gray-700 justify-self-start'>{props.msg}</div>
+			<div className='col-span-2 row-span-4 m-auto text-xl text-justify text-gray-700 justify-self-start'>
+				<p>{props.msg}</p>
+			</div>
 		</div>
 	)
 }
