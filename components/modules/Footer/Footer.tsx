3701ed3b443fa35815d851f6876ce502a0405356
Abhinav Robinson <abhinavrobinson@gmail.com>
2021-01-29 19:53:19 +0530

    (feat): footer responsive

--- components/modules/Footer/Footer.tsx
+++ components/modules/Footer/Footer.tsx
@@ -22,9 +22,9 @@ const Footer = (props: any) => {
 
 	const styles = {
 		logo: 'text-4xl font-semibold text-white',
-		container: 'bg-primary flex text-left justify-evenly px-14 py-20',
+		container: 'bg-primary flex flex-col md:flex-row text-left justify-evenly px-14 py-20',
 		menu: {
-			header: 'font-bold text-lg p-2 pb-8 text-white flex flex-row',
+			header: 'font-bold text-lg p-2 py-8 text-white flex flex-row',
 			icon: 'px-2 text-2xl text-secondary-light',
 			container: 'flex flex-col items-right space-x-5',
 			text: 'text-gray-400 font-medium no-underline hover:text-secondary-light',
@@ -33,7 +33,7 @@ const Footer = (props: any) => {
 		copy: 'text-white no-underline text-center w-full',
 		search: {
 			box: 'flex flex-col justify-start mt-5 md:w-1/3 lg:w-1/4 lg:ml-2 lg:mr-10 lg:mt-0',
-			header: 'text-xl font-semibold text-white',
+			header: 'text-xl font-semibold pt-4 text-white',
 			subheader: 'py-4 text-sm font-semibold text-blue-300',
 			terms: {
 				box: 'py-4',
