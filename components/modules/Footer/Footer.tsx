1a64e26e64ec50e43212d24f3ef969434bace3a8
Abhinav Robinson <abhinavrobinson@gmail.com>
2021-01-29 20:00:51 +0530

    (feat): footer repo change

--- components/modules/Footer/Footer.tsx
+++ components/modules/Footer/Footer.tsx
@@ -24,7 +24,7 @@ const Footer = (props: any) => {
 		logo: 'text-4xl font-semibold text-white',
 		container: 'bg-primary flex flex-col md:flex-row text-left justify-evenly px-14 py-20',
 		menu: {
-			header: 'font-bold text-lg p-2 py-8 text-white flex flex-row',
+			header: 'font-bold text-lg p-2 py-8 md:pt-0 text-white flex flex-row',
 			icon: 'px-2 text-2xl text-secondary-light',
 			container: 'flex flex-col items-right space-x-5',
 			text: 'text-gray-400 font-medium no-underline hover:text-secondary-light',
@@ -33,11 +33,11 @@ const Footer = (props: any) => {
 		copy: 'text-white no-underline text-center w-full',
 		search: {
 			box: 'flex flex-col justify-start mt-5 md:w-1/3 lg:w-1/4 lg:ml-2 lg:mr-10 lg:mt-0',
-			header: 'text-xl font-semibold pt-4 text-white',
+			header: 'text-xl font-semibold pt-4 md:pt-0 text-white',
 			subheader: 'py-4 text-sm font-semibold text-blue-300',
 			terms: {
 				box: 'py-4',
-				check: 'py-4',
+				check: 'py-5',
 			},
 		},
 	}
