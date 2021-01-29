011205464b71f829598afd5161c880845b5ec649
Abhinav Robinson <abhinavrobinson@gmail.com>
2021-01-29 17:45:35 +0530

    (feat): start footer adjustments

--- components/modules/Footer/Footer.tsx
+++ components/modules/Footer/Footer.tsx
@@ -5,12 +5,16 @@ import SearchCompact from '../Search/SearchCompact.module'
  */
 const Footer = (props: any) => {
 	const links = [
-		{ href: '#', label: 'Home' },
-		{ href: '#', label: 'Companies' },
-		{ href: '#', label: 'Services' },
-		{ href: '#', label: 'Search' },
-		{ href: '#', label: 'Register' },
-		{ href: '#', label: 'Login' },
+		{ href: '#', label: 'BrainBrush' },
+		{ href: '#', label: 'About Us' },
+		{ href: '#', label: 'Privacy Policy' },
+		{ href: '#', label: 'FAQs' },
+		{ href: '#', label: 'Glossary' },
+		{ href: '#', label: 'Contact Us' },
+		{ href: '#', label: 'Submit Your Company' },
+		{ href: '#', label: 'Advertise with Us' },
+		{ href: '#', label: 'Directory' },
+		{ href: '#', label: 'Blogs' },
 	]
 
 	const styles = {
@@ -20,7 +24,7 @@ const Footer = (props: any) => {
 			container: 'flex items-center justify-between space-x-5',
 			text: 'text-white no-underline hover:text-secondary-light',
 		},
-		copy: 'text-white no-underline text-center w-full pt-16 lg:border-t lg:border-gray-500',
+		copy: 'text-white no-underline text-center w-full pt-16',
 	}
 
 	return (
@@ -37,11 +41,6 @@ const Footer = (props: any) => {
 				</ul>
 				<div className='flex justify-center mt-5 lg:w-1/3 lg:ml-2 lg:mr-10 lg:mt-0'>
 					<SearchCompact msg='Why not subscribe? We don`t spam :)' expand='true' button='Subscribe' />
-				</div>
-				<div className='flex items-center justify-between py-5 text-white lg:py-0'>
-					<div className='mx-2 text-center'>Contact Us</div>
-					<div className='mx-2 text-center'>Privacy Policy</div>
-					<div className='mx-2 text-center'>Our Terms & Conditions</div>
 				</div>
 			</div>
 			<div className={styles.container}>
