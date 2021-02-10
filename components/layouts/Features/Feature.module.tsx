5b1142442280822a79a440e19a412ed8e6c5dd4a
Abhinav Robinson <abhinavrobinson@gmail.com>
2021-02-10 20:58:53 +0530

    (feat): features edit

--- components/layouts/Features/Feature.module.tsx
+++ components/layouts/Features/Feature.module.tsx
@@ -11,23 +11,17 @@ const Feature = (props: any) => {
 
 	return (
 		<div
-			className='grid grid-cols-1 gap-5 p-10 duration-200 bg-transparent lg:h-full lg:w-2/3 lg:grid-cols-2 start align-items-center rounded-2xl'
+			className='grid grid-cols-1 gap-5 p-10 pt-32 duration-200 bg-transparent lg:h-full lg:w-2/3 lg:grid-cols-2 start align-items-center rounded-2xl'
 			id='feature-card'
 		>
 			<div id='text' className='flex flex-col max-h-max'>
-				<div className='pb-10 text-4xl'>Feature Name</div>
-				<div className='text-lg text-justify'>
+				<div className='mb-24 font-thin text-7xl'>Feature Name</div>
+				<div style={{ height: '3px', width: '300px', backgroundColor: '#333' }} className='mb-24'></div>
+				<div className='text-3xl font-thin leading-relaxed text-justify'>
 					Lorem ipsum dolor sit amet consectetur adipisicing elit. Nulla quaerat perspiciatis incidunt voluptatum mollitia corporis
 					accusamus impedit sint voluptate? Debitis dignissimos vitae veritatis. Mollitia laborum aperiam id rerum excepturi illo neque
-					animi rem quo perferendis officiis vitae corrupti perspiciatis consequuntur, quibusdam nemo itaque quia vel accusamus. Laboriosam
-					magnam dolores corporis!
+					animi rem quo perferendis officiis vitae corrupti perspiciatis consequuntur, quibusdam nemo itaque quia vel accusamus.
 				</div>
-				<br />
-				<div className='text-lg text-justify'>Lorem ipsum dolor adipisicing elit.</div>
-				<br />
-				<div className='text-lg text-justify'>Lorem ipsum dolor sit amet consectetur adipisicing elit.</div>
-				<br />
-				<div className='text-lg text-justify'>Lorem ipsum dolor sit amet consectetur adipisicing cumque non?</div>
 			</div>
 			<div id='image' className={image}>
 				{props.img}
