dcd6af70356b7e9f1dac7742c330c64a31453f5c
Abhinav Robinson <abhinavrobinson@gmail.com>
2021-02-10 21:00:24 +0530

    (feat): size fix

--- components/layouts/Features/Feature.module.tsx
+++ components/layouts/Features/Feature.module.tsx
@@ -11,13 +11,13 @@ const Feature = (props: any) => {
 
 	return (
 		<div
-			className='grid grid-cols-1 gap-5 p-10 pt-32 duration-200 bg-transparent lg:h-full lg:w-2/3 lg:grid-cols-2 start align-items-center rounded-2xl'
+			className='grid grid-cols-1 gap-5 p-10 duration-200 bg-transparent pt-22 lg:h-full lg:w-2/3 lg:grid-cols-2 start align-items-center rounded-2xl'
 			id='feature-card'
 		>
 			<div id='text' className='flex flex-col max-h-max'>
-				<div className='mb-24 font-thin text-7xl'>Feature Name</div>
-				<div style={{ height: '3px', width: '300px', backgroundColor: '#333' }} className='mb-24'></div>
-				<div className='text-3xl font-thin leading-relaxed text-justify'>
+				<div className='mb-24 text-6xl font-thin'>Feature Name</div>
+				<div style={{ height: '3px', width: '250px', backgroundColor: '#333' }} className='mb-24'></div>
+				<div className='text-xl font-thin leading-relaxed text-justify'>
 					Lorem ipsum dolor sit amet consectetur adipisicing elit. Nulla quaerat perspiciatis incidunt voluptatum mollitia corporis
 					accusamus impedit sint voluptate? Debitis dignissimos vitae veritatis. Mollitia laborum aperiam id rerum excepturi illo neque
 					animi rem quo perferendis officiis vitae corrupti perspiciatis consequuntur, quibusdam nemo itaque quia vel accusamus.
