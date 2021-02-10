b126c8f0d9316c7e70676c419891fe915ca1de7c
Abhinav Robinson <abhinavrobinson@gmail.com>
2021-02-10 21:26:24 +0530

    (feat):giving it some air

--- components/layouts/Reviews/Reviews.tsx
+++ components/layouts/Reviews/Reviews.tsx
@@ -6,7 +6,7 @@ import Review from './Review.module'
  */
 const Reviews = () => {
 	return (
-		<div className='flex flex-col justify-center w-full p-4 pt-10 lg:p-20'>
+		<div className='flex flex-col justify-center w-full p-4 pt-10 lg:p-20 lg:py-48'>
 			<div className='p-6 mb-20 text-5xl font-semibold text-center lg:p-0 text-primary header '>Here from our Customers!</div>
 			<div className='flex flex-row flex-wrap justify-center m-auto lg:gap-28 blogs'>
 				<Review
