c0c089e125e2b549d08455c7abc730110ae89b44
aaryan-kapur <ak6392@bennett.edu.in>
2021-02-12 00:43:51 +0530

    change stuff.

--- components/layouts/Reviews/Review.module.tsx
+++ components/layouts/Reviews/Review.module.tsx
@@ -5,7 +5,13 @@
 const Review = (props: any) => {
 	return (
 		<div className=''>
-			<div className={props.center} id='blog-card'>
+			<div
+				className={
+					'  review  grid grid-cols-2 grid-rows-7 gap-5 p-10 mx-2 mb-10 text-center duration-300   border-2 border-grey-600 hover:shadow-xl w-400px lg:mb-0 align-items-center rounded-2xl ' +
+					props.center
+				}
+				id='blog-card'
+			>
 				<div className='text-3xl font-semibold  justify-self-start   text-800'>{props.companyName}</div>
 				<div className='col-span-2 row-span-4 m-auto text-xl text-justify  justify-self-start  text-700'>
 					<p>{props.msg}</p>
--- components/layouts/Reviews/Reviews.tsx
+++ components/layouts/Reviews/Reviews.tsx
@@ -14,19 +14,19 @@ const Reviews = () => {
 					companyName='Miran A.'
 					msg='Lorem ipsum dolor sit amet consectetur adipisicing elit. Et commodi expedita qui perspiciatis, tenetur nostrum facilis quisquam ex aspernatur quam! Blanditiis harum eos rerum cum illo nemo dolorem illum voluptas maiores reiciendis atque, alias explicabo laboriosam aut unde iure eveniet quod iste quasi sit. Dolorem eius asperiores nemo illum placeat!'
 					color='bg-yellow-300'
-					center='bg-white  grid grid-cols-2 grid-rows-7 gap-5 p-10 mx-2 mb-10 text-center duration-300   border-2 border-grey-600 hover:shadow-xl w-400px lg:mb-0 align-items-center rounded-2xl '
+					center='bg-white'
 				/>
 				<Review
 					companyName='John R.'
 					msg='Lorem ipsum dolor sit amet consectetur adipisicing elit. Et commodi expedita qui perspiciatis, tenetur nostrum facilis quisquam ex aspernatur quam! Blanditiis harum eos rerum cum illo nemo dolorem illum voluptas maiores reiciendis atque, alias explicabo laboriosam aut unde iure eveniet quod iste quasi sit. Dolorem eius asperiores nemo illum placeat!'
 					color='bg-gray-400'
-					center='bg-red grid grid-cols-2 grid-rows-4 gap-5 p-10 mx-2 mb-10 text-center duration-300 whitetext  border-2 border-white-400 hover:shadow-xl w-400px lg:mb-0 align-items-center rounded-2xl'
+					center='bg-red whitetext'
 				/>
 				<Review
 					companyName='Bella B.'
 					msg='Lorem ipsum dolor sit amet consectetur adipisicing elit. Et commodi expedita qui perspiciatis, tenetur nostrum facilis quisquam ex aspernatur quam! Blanditiis harum eos rerum cum illo nemo dolorem illum voluptas maiores reiciendis atque, alias explicabo laboriosam aut unde iure eveniet quod iste quasi sit. Dolorem eius asperiores nemo illum placeat!'
 					color='bg-blue-400'
-					center='bg-white grid grid-cols-2 grid-rows-7 gap-5 p-10 mx-2 mb-10 text-center duration-300   border-2 border-grey-600 hover:shadow-xl w-400px lg:mb-0 align-items-center rounded-2xl'
+					center='bg-white'
 				/>
 			</div>
 		</div>
