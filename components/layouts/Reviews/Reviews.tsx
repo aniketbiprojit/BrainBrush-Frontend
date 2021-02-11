87042c845aa6e83d7589c5cf188ad475c2c5dddf
aaryan-kapur <ak6392@bennett.edu.in>
2021-02-12 00:58:55 +0530

    change stuff.

--- components/layouts/Reviews/Reviews.tsx
+++ components/layouts/Reviews/Reviews.tsx
@@ -8,7 +8,7 @@ import Zoom from 'react-reveal/Zoom'
 const Reviews = () => {
 	return (
 		<div className='flex flex-col justify-center w-full p-4 pt-10 lg:p-20 lg:py-48'>
-			<div className='p-6 mb-20 text-5xl font-semibold text-center lg:p-0 text-primary header '>Here from our Customers!</div>
+			<div className='p-6 mb-20 text-5xl font-semibold text-center lg:p-0 text-primary header '>Hear from our Customers!</div>
 			<div className='flex flex-row flex-wrap justify-center m-auto lg:gap-28 blogs'>
 				<Review
 					companyName='Miran A.'
