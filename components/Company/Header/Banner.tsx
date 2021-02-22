d430cfb111a04be9c44415ff15eb58453ad20cb2
Abhinav Robinson <abhinavrobinson@gmail.com>
2021-02-22 13:05:37 +0530

    (feat): add sub head

--- components/Company/Header/Banner.tsx
+++ components/Company/Header/Banner.tsx
@@ -2,9 +2,13 @@ import React from 'react'
 
 const Banner: React.FC<{ company: string }> = ({ company }) => {
 	return (
-		<>
-			<h1>{company}</h1>
-		</>
+		<div className='flex px-56 py-28'>
+			<h1 className='text-5xl font-semibold'>{company}</h1>
+			<div className=''>
+				<h2>GOOG</h2>
+				<h2>NASDAQ</h2>
+			</div>
+		</div>
 	)
 }
 
