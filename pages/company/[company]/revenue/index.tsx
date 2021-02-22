33ba8aef58575e3646ae39de6e75424116c9b600
Aniket Biprojit Chowdhury <aniket_chowdhury@hotmail.com>
2021-02-22 12:20:03 +0530

    feat: allow woute

--- pages/company/[company]/revenue/index.tsx
+++ pages/company/[company]/revenue/index.tsx
@@ -7,16 +7,13 @@ const Revenue: React.FC<{ company: string }> = ({ company }) => {
 	const { route } = useRouter()
 	return (
 		<Fragment>
-			{route === '/company/[company]/revenue' ? (
-				<Fragment>Absolute Page {route}</Fragment>
-			) : (
-				<Fragment>
-					Relative Page Link to Absolute
-					<Link href={`/company/${company}/revenue`}>
-						<p>Page</p>
-					</Link>
-				</Fragment>
-			)}
+			{route === '/company/[company]/revenue' && <Fragment>Header, Navbar, etc. {route}</Fragment>}
+			<Fragment>
+				Relative Page Link to Absolute
+				<Link href={`/company/${company}/revenue`}>
+					<p>Page</p>
+				</Link>
+			</Fragment>
 		</Fragment>
 	)
 }
