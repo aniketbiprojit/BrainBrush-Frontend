c0c70f99f48e4d1234c6e8d7d60ee15b8adb3e6f
Aniket Biprojit Chowdhury <aniket_chowdhury@hotmail.com>
2021-02-22 14:32:34 +0530

    feat: get_data

--- pages/company/[company]/index.tsx
+++ pages/company/[company]/index.tsx
@@ -1,12 +1,27 @@
-import { useRouter } from 'next/dist/client/router'
 import React, { Fragment } from 'react'
+import { GetServerSideProps } from 'next'
+import { InferGetServerSidePropsType } from 'next'
 
 import CompanyHeader from '../../../components/Company/Header'
 import Revenue from './revenue'
 
-const Company: React.FC = () => {
-	const router = useRouter()
-	const company = router.query.company as string
+export const getServerSideProps: GetServerSideProps = async (ctx) => {
+	// console.log(ctx.query?.company)
+	return {
+		props: {
+			company_data: {
+				ticker: 'GOOG',
+				stock_market: 'NASDAQ',
+				company: ctx.query.company,
+			},
+			// icon: <FaGoogle className='mr-10 text-8xl center' />,
+		},
+	}
+}
+
+const Company: React.FC<{ company_data: InferGetServerSidePropsType<typeof getServerSideProps> }> = ({ company_data }) => {
+	const company = company_data.company
+
 	return (
 		<Fragment>
 			<CompanyHeader company={company} />
