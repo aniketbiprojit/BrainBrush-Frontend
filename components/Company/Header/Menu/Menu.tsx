beea22eb58287c94a1331fb0a7afebace8c31981
Abhinav Robinson <abhinavrobinson@gmail.com>
2021-02-22 15:13:52 +0530

    (fix): overflow respo menu cards

--- components/Company/Header/Menu/Menu.tsx
+++ components/Company/Header/Menu/Menu.tsx
@@ -4,8 +4,8 @@ import ButtonCard from './ButtonCard/ButtonCard'
 
 const Menu: React.FC<{ currentTab: string }> = ({ currentTab }) => {
 	return (
-		<div className='w-full py-12 bg-white md:px-28'>
-			<ul className='flex flex-wrap justify-center w-full gap-0 md:gap-10'>
+		<div className='w-full py-12 bg-white md:px-18'>
+			<ul className='flex flex-wrap justify-center w-full gap-0 md:gap-8'>
 				<ButtonCard icon={<FaSearch />} text='Overview' isSelected={false} />
 				<ButtonCard icon={<FaDollarSign />} text='Revenue' isSelected={true} />
 				<ButtonCard icon={<FaCoins />} text='Financials' isSelected={false} />
