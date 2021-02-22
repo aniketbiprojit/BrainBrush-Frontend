7056f41fe73f413f18266af54d691a501a2356b2
Aniket Biprojit Chowdhury <aniket_chowdhury@hotmail.com>
2021-02-22 13:10:04 +0530

    feat: ticker and stock_market

--- components/Company/Header/Banner.tsx
+++ components/Company/Header/Banner.tsx
@@ -1,15 +1,23 @@
 import React from 'react'
 import { FaGoogle } from 'react-icons/fa'
 
+function get_company_data(company: string) {
+	return {
+		ticker: 'GOOG',
+		stock_market: 'NASDAQ',
+	}
+}
 const Banner: React.FC<{ company: string }> = ({ company }) => {
+	const { ticker, stock_market } = get_company_data(company)
+
 	return (
 		<div className='flex px-56 py-28'>
 			<FaGoogle className='mr-10 align-middle text-8xl center' />
 			<div className='flex flex-col'>
 				<h1 className='text-5xl font-semibold'>{company}</h1>
 				<div className='flex flex-row gap-10 pt-5'>
-					<h2>GOOG</h2>
-					<h2>NASDAQ</h2>
+					<h2>{ticker}</h2>
+					<h2>{stock_market}</h2>
 				</div>
 			</div>
 		</div>
