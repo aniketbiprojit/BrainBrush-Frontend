4660e333ee54b5946dc6adf84f5a274b3611da85
Abhinav Robinson <abhinavrobinson@gmail.com>
2021-02-22 19:10:29 +0530

    (fix): navbar actions#2

--- pages/index.tsx
+++ pages/index.tsx
@@ -36,18 +36,18 @@ class HomePage extends React.Component<PropsFromRedux & {}> {
 		console.log(this.props.ApplicationState)
 
 		window.addEventListener('scroll', (_) => {
-			if (window.pageYOffset >= document.getElementById('features').offsetTop - window.innerHeight / 2) {
-				document.getElementById('nav-container').classList.add('bg-gray-900')
-				document.getElementById('nav-container').classList.add('shadow-xl')
-				document.getElementById('nav-container').classList.remove('bg-transparent')
-				document.getElementById('searchbar').classList.add('sm:flex')
-			} else {
-				document.getElementById('nav-container').classList.add('bg-transparent')
-				document.getElementById('nav-container').classList.remove('bg-gray-900')
-				document.getElementById('nav-container').classList.remove('shadow-xl')
-				document.getElementById('searchbar').classList.add('hidden')
-				document.getElementById('searchbar').classList.remove('sm:flex')
-			}
+			// if (window.pageYOffset >= document.getElementById('features').offsetTop - window.innerHeight / 2) {
+			// 	document.getElementById('nav-container').classList.add('bg-gray-900')
+			// 	document.getElementById('nav-container').classList.add('shadow-xl')
+			// 	document.getElementById('nav-container').classList.remove('bg-transparent')
+			// 	document.getElementById('searchbar').classList.add('sm:flex')
+			// } else {
+			// 	document.getElementById('nav-container').classList.add('bg-transparent')
+			// 	document.getElementById('nav-container').classList.remove('bg-gray-900')
+			// 	document.getElementById('nav-container').classList.remove('shadow-xl')
+			// 	document.getElementById('searchbar').classList.add('hidden')
+			// 	document.getElementById('searchbar').classList.remove('sm:flex')
+			// }
 		})
 	}
 
