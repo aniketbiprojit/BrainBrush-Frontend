0f1b074560a62a9a1c34336457f9235e408e5db2
Aniket Biprojit Chowdhury <aniket_chowdhury@hotmail.com>
2021-02-22 19:26:36 +0530

    feat: external

--- components/Company/Revenue/Layout.tsx
+++ components/Company/Revenue/Layout.tsx
@@ -1,6 +1,8 @@
-import React from 'react'
+import React, { Fragment } from 'react'
 
 import Box from '../../lib/Box/Box'
+
+const companies = ['Netflix', 'Amazon', 'Google', 'Lenovo']
 
 const RevenueLayout = () => {
 	return (
@@ -13,7 +15,7 @@ const RevenueLayout = () => {
 					<div className=''></div>
 				</Box>
 				<Box heading='Related'>
-					<div className=''></div>
+					<Related></Related>
 				</Box>
 			</div>
 		</>
@@ -21,3 +23,18 @@ const RevenueLayout = () => {
 }
 
 export default RevenueLayout
+
+const Related: React.FC = () => {
+	return (
+		<Fragment>
+			{companies.map((company) => {
+				return (
+					<div className='flex'>
+						<div className='circle p-2'></div>
+						<div className='p-2'>{company}</div>
+					</div>
+				)
+			})}
+		</Fragment>
+	)
+}
