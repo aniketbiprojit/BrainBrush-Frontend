a8e2dca88b8fbc11d8dd184aa6c81490d162e5fd
Abhinav Robinson <abhinavrobinson@gmail.com>
2021-02-22 15:12:26 +0530

    (feat): respo menu cards#2

--- components/Company/Header/Menu/ButtonCard/ButtonCard.tsx
+++ components/Company/Header/Menu/ButtonCard/ButtonCard.tsx
@@ -1,8 +1,8 @@
 import React from 'react'
 const ButtonCard: React.FC<{ icon: any; text: string; isSelected: boolean }> = ({ icon, text, isSelected }) => {
 	return (
-		<div className={`p-4 md:p-20 justify-center ${isSelected ? 'bg-secondary' : 'hover:bg-gray-300'} rounded-3xl`}>
-			<div className={`icon ml-6 md:ml-2 justify-center md:text-8xl ${isSelected ? 'text-white' : ''}`}>{icon}</div>
+		<div className={`p-4 m-2 my-4 md:p-20 justify-center ${isSelected ? 'bg-secondary' : 'hover:bg-gray-300'} rounded-xl md:rounded-3xl`}>
+			<div className={`icon ml-3 md:ml-2 justify-center text-4xl md:text-8xl ${isSelected ? 'text-white' : ''}`}>{icon}</div>
 			<div className={`mt-4 md:mt-12 text-sm md:text-2xl font-semibold ${isSelected ? 'text-white' : ''}`}>{text}</div>
 		</div>
 	)
