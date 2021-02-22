79108e765c75a9fc5caa55a9147b4a70f337a7fa
Abhinav Robinson <abhinavrobinson@gmail.com>
2021-02-22 13:35:17 +0530

    (test): add build

---
+++ out/_next/static/chunks/pages/index-3ce8d1444d689b6c60ec.js
@@ -0,0 +1,1 @@
+_N_E=(window.webpackJsonp_N_E=window.webpackJsonp_N_E||[]).push([[18],{"/EDR":function(_,n,w){(window.__NEXT_P=window.__NEXT_P||[]).push(["/",function(){return w("23aj")}])}},[["/EDR",1,2,5,6,0,3,4,7,9]]]);
