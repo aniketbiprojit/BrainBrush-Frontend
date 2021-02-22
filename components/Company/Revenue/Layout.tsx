666c001af381897f8224812c80932dca5c352f05
Aniket Biprojit Chowdhury <aniket_chowdhury@hotmail.com>
2021-02-22 19:40:09 +0530

    feat: multiple companies

--- components/Company/Revenue/Layout.tsx
+++ components/Company/Revenue/Layout.tsx
@@ -2,7 +2,14 @@ import React, { Fragment } from 'react'
 
 import Box from '../../lib/Box/Box'
 
-const companies = ['Netflix', 'Amazon', 'Google', 'Lenovo']
+const companies = [
+	{ name: 'Netflix', color: '#FF5A5A' },
+	{ name: 'Amazon', color: '#3FA7D6' },
+	{ name: 'Google', color: '#A8DADC' },
+	{ name: 'Lenovo', color: '#EF0000' },
+	{ name: 'Samsung', color: '#FFC327' },
+	{ name: 'Microsoft', color: '#C4C4C4' },
+]
 
 const RevenueLayout = () => {
 	return (
@@ -29,9 +36,12 @@ const Related: React.FC = () => {
 		<Fragment>
 			{companies.map((company) => {
 				return (
-					<div className='flex'>
-						<div className='circle p-2'></div>
-						<div className='p-2'>{company}</div>
+					<div className='flex' style={{ alignItems: 'center' }}>
+						<div
+							style={{ width: '27px', height: '27px', borderRadius: '50%', backgroundColor: `${company.color}` }}
+							className='my-4'
+						></div>
+						<div className='m-2 text-2xl'>{company.name}</div>
 					</div>
 				)
 			})}
