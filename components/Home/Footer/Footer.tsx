b9843a1cc33b0f5db6da9867de88d4707adbe7d1
aaryan-kapur <ak6392@bennett.edu.in>
2021-02-22 18:29:22 +0530

    chota change

--- components/Home/Footer/Footer.tsx
+++ components/Home/Footer/Footer.tsx
@@ -45,7 +45,7 @@ const Footer = () => {
 
 	return (
 		<>
-			<footer className={styles.container + ' py-20'}>
+			<footer className={styles.container}>
 				<h1 className={styles.logo}>Brainbrush</h1>
 				<ul className={styles.menu.container} id='footerList'>
 					<li className={styles.menu.header}>
