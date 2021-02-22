afd6b015067fd1b7e3a8a24a4a10af42ca1981bc
Abhinav Robinson <abhinavrobinson@gmail.com>
2021-02-22 14:44:52 +0530

    (feat): menu cards#2

--- components/Company/Header/Menu/ButtonCard/ButtonCard.tsx
+++ components/Company/Header/Menu/ButtonCard/ButtonCard.tsx
@@ -1,9 +1,9 @@
 import React from 'react'
-const ButtonCard: React.FC<{ icon: any; text: string }> = ({ icon, text }) => {
+const ButtonCard: React.FC<{ icon: any; text: string; isSelected: boolean }> = ({ icon, text, isSelected }) => {
 	return (
-		<div className='p-10 bg-gray-500 rounded-3xl'>
-			<div className='icon'>{icon}</div>
-			<div className='mt-8 text-2xl'>{text}</div>
+		<div className={`p-20 justify-items-center items-center ${isSelected ? 'bg-secondary hover:bg-blue-300' : 'hover:bg-gray-300'} rounded-3xl`}>
+			<div className={`icon ${isSelected ? 'text-white' : ''}`}>{icon}</div>
+			<div className={`mt-8 text-2xl font-semibold ${isSelected ? 'text-white' : ''}`}>{text}</div>
 		</div>
 	)
 }
--- components/Company/Header/Menu/Menu.tsx
+++ components/Company/Header/Menu/Menu.tsx
@@ -1,12 +1,14 @@
 import React from 'react'
-import { FaSearch } from 'react-icons/fa'
+import { FaCoins, FaDollarSign, FaSearch } from 'react-icons/fa'
 import ButtonCard from './ButtonCard/ButtonCard'
 
 const Menu: React.FC<{ currentTab: string }> = ({ currentTab }) => {
 	return (
 		<div className='w-full py-12 bg-white px-28'>
 			<ul className='flex'>
-				<ButtonCard icon={<FaSearch className='text-black text-8xl' />} text='Overview' />
+				<ButtonCard icon={<FaSearch className='text-8xl' />} text='Overview' isSelected={true} />
+				<ButtonCard icon={<FaDollarSign className='text-8xl' />} text='Revenue' isSelected={false} />
+				<ButtonCard icon={<FaCoins className='text-8xl' />} text='Financials' isSelected={false} />
 			</ul>
 		</div>
 	)
