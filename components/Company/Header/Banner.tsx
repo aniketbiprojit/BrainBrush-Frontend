d607302397d67e4405f5d8954d30324e74fe9728
Abhinav Robinson <abhinavrobinson@gmail.com>
2021-02-22 13:10:05 +0530

    (feat): add logo and struct

--- components/Company/Header/Banner.tsx
+++ components/Company/Header/Banner.tsx
@@ -1,12 +1,16 @@
 import React from 'react'
+import { FaGoogle } from 'react-icons/fa'
 
 const Banner: React.FC<{ company: string }> = ({ company }) => {
 	return (
 		<div className='flex px-56 py-28'>
-			<h1 className='text-5xl font-semibold'>{company}</h1>
-			<div className=''>
-				<h2>GOOG</h2>
-				<h2>NASDAQ</h2>
+			<FaGoogle className='mr-10 align-middle text-8xl center' />
+			<div className='flex flex-col'>
+				<h1 className='text-5xl font-semibold'>{company}</h1>
+				<div className='flex flex-row gap-10 pt-5'>
+					<h2>GOOG</h2>
+					<h2>NASDAQ</h2>
+				</div>
 			</div>
 		</div>
 	)
