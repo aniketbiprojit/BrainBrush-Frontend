2f20c687a93b98e9b4d8cd4abaa3d166e81e3b18
Abhinav Robinson <abhinavrobinson@gmail.com>
2021-02-25 21:33:41 +0530

    (feat): menu respo

--- components/Company/Header/Menu/ButtonCard/ButtonCard.tsx
+++ components/Company/Header/Menu/ButtonCard/ButtonCard.tsx
@@ -2,15 +2,17 @@ import React from 'react'
 const ButtonCard: React.FC<{ icon: any; text: string; isSelected: boolean; minimize?: boolean }> = ({ icon, text, isSelected, minimize = false }) => {
 	return (
 		<div
-			style={{ cursor: 'pointer', padding: '1.5vw' }}
-			className={`${isSelected ? 'bg-secondary' : 'hover:bg-gray-300'} rounded md:rounded-3xl`}
+			style={{ cursor: 'pointer', padding: '1vw', minWidth: '10vw' }}
+			className={`${isSelected ? 'bg-secondary' : 'hover:bg-gray-300'} rounded md:rounded-xl`}
 		>
 			<div className={`icon grid justify-items-center ${isSelected ? 'text-white' : ''}`} style={{ fontSize: '3vw' }}>
 				{icon}
 			</div>
-			<div className={`font-semibold ${isSelected ? 'text-white' : ''}`} style={{ marginTop: '2vw', fontSize: '1.5vw' }}>
-				{text}
-			</div>
+			{!minimize && (
+				<div className={`font-semibold text-center ${isSelected ? 'text-white' : ''}`} style={{ marginTop: '2vw', fontSize: '1.5vw' }}>
+					{text}
+				</div>
+			)}
 		</div>
 	)
 }
