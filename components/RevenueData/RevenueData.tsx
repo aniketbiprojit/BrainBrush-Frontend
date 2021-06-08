66780ea4ed4a3bf6f07081f1b80714ea57e91b38
Shamanta Sristy <shamantasristy@gmail.com>
2021-06-08 07:54:07 +0600

    errors on initial textbox and bar chart

---
+++ components/RevenueData/RevenueData.tsx
@@ -0,0 +1,112 @@
+//paragraph intro data
+export const introdata =
+	'The below chart shows Apple’s annual revenue starting from $65.234 Billion in 2010 to $274.51 Billion in 2020 Throughout the period, Apple’s revenue has been increasing year-over-year and only declined in 2016 but rose back in 2017 and then dipped in 2019. The largest dip was in 2016 and followed by one of the largest revenues jumps the year 2018. In the fiscal year 2020, Apple’s annual revenue was $274.17 Billion, which increased +5.5% compared to 2019 In the fiscal year 2019, Apple’s annual revenue was $260.17 Billion, which declined -2.04% compared to 2018 To learn more about revenue, its formula and definition, check out our glossary. The below chart shows Apple’s annual revenue starting from $65.234 Billion in 2010 to $274.51 Billion in 2020 Throughout the period, Apple’s revenue has been increasing year-over-year and only declined in 2016 but rose back in 2017 and then dipped in 2019. The largest dip was in 2016 and followed by one of the largest revenues jumps the year 2018. In the fiscal year 2020, Apple’s annual revenue was $274.17 Billion, which increased +5.5% compared to 2019 In the fiscal year 2019, Apple’s annual revenue was $260.17 Billion, which declined -2.04% compared to 2018 To learn more about revenue, its formula and definition, check out our glossary '
+
+    // Bar chart data
+    export const annualData = [
+        {
+          "country": "AD",
+          "hot dog": 165,
+          "hot dogColor": "hsl(79, 70%, 50%)",
+          "burger": 85,
+          "burgerColor": "hsl(207, 70%, 50%)",
+          "sandwich": 15,
+          "sandwichColor": "hsl(119, 70%, 50%)",
+          "kebab": 48,
+          "kebabColor": "hsl(326, 70%, 50%)",
+          "fries": 112,
+          "friesColor": "hsl(96, 70%, 50%)",
+          "donut": 148,
+          "donutColor": "hsl(214, 70%, 50%)"
+        },
+        {
+          "country": "AE",
+          "hot dog": 10,
+          "hot dogColor": "hsl(294, 70%, 50%)",
+          "burger": 64,
+          "burgerColor": "hsl(84, 70%, 50%)",
+          "sandwich": 69,
+          "sandwichColor": "hsl(23, 70%, 50%)",
+          "kebab": 116,
+          "kebabColor": "hsl(206, 70%, 50%)",
+          "fries": 59,
+          "friesColor": "hsl(22, 70%, 50%)",
+          "donut": 185,
+          "donutColor": "hsl(236, 70%, 50%)"
+        },
+        {
+          "country": "AF",
+          "hot dog": 30,
+          "hot dogColor": "hsl(251, 70%, 50%)",
+          "burger": 151,
+          "burgerColor": "hsl(35, 70%, 50%)",
+          "sandwich": 152,
+          "sandwichColor": "hsl(92, 70%, 50%)",
+          "kebab": 56,
+          "kebabColor": "hsl(127, 70%, 50%)",
+          "fries": 188,
+          "friesColor": "hsl(137, 70%, 50%)",
+          "donut": 65,
+          "donutColor": "hsl(18, 70%, 50%)"
+        },
+        {
+          "country": "AG",
+          "hot dog": 124,
+          "hot dogColor": "hsl(322, 70%, 50%)",
+          "burger": 72,
+          "burgerColor": "hsl(74, 70%, 50%)",
+          "sandwich": 93,
+          "sandwichColor": "hsl(287, 70%, 50%)",
+          "kebab": 73,
+          "kebabColor": "hsl(167, 70%, 50%)",
+          "fries": 61,
+          "friesColor": "hsl(97, 70%, 50%)",
+          "donut": 177,
+          "donutColor": "hsl(172, 70%, 50%)"
+        },
+        {
+          "country": "AI",
+          "hot dog": 7,
+          "hot dogColor": "hsl(148, 70%, 50%)",
+          "burger": 163,
+          "burgerColor": "hsl(1, 70%, 50%)",
+          "sandwich": 38,
+          "sandwichColor": "hsl(65, 70%, 50%)",
+          "kebab": 80,
+          "kebabColor": "hsl(171, 70%, 50%)",
+          "fries": 144,
+          "friesColor": "hsl(277, 70%, 50%)",
+          "donut": 173,
+          "donutColor": "hsl(194, 70%, 50%)"
+        },
+        {
+          "country": "AL",
+          "hot dog": 27,
+          "hot dogColor": "hsl(251, 70%, 50%)",
+          "burger": 113,
+          "burgerColor": "hsl(51, 70%, 50%)",
+          "sandwich": 78,
+          "sandwichColor": "hsl(5, 70%, 50%)",
+          "kebab": 94,
+          "kebabColor": "hsl(123, 70%, 50%)",
+          "fries": 65,
+          "friesColor": "hsl(22, 70%, 50%)",
+          "donut": 122,
+          "donutColor": "hsl(286, 70%, 50%)"
+        },
+        {
+          "country": "AM",
+          "hot dog": 161,
+          "hot dogColor": "hsl(327, 70%, 50%)",
+          "burger": 114,
+          "burgerColor": "hsl(251, 70%, 50%)",
+          "sandwich": 4,
+          "sandwichColor": "hsl(54, 70%, 50%)",
+          "kebab": 33,
+          "kebabColor": "hsl(121, 70%, 50%)",
+          "fries": 196,
+          "friesColor": "hsl(161, 70%, 50%)",
+          "donut": 104,
+          "donutColor": "hsl(10, 70%, 50%)"
+        }
+      ]
--- pages/company/[company]/index.tsx
+++ pages/company/[company]/index.tsx
@@ -1,13 +1,29 @@
-import React, { Fragment } from 'react'
+import React, { Fragment, useState } from 'react'
 
 import { GetServerSideProps, InferGetServerSidePropsType } from 'next'
+import { Button, Container } from 'react-bootstrap'
+import Bar from '../../../components/Charts/Bar'
+import { InitialTextBox } from '../../../components/InitialTextBox/InitialTextBox'
+// import Bar from '../../../components/Charts/Bar'
 
 export const getServerSideProps: GetServerSideProps = async (context) => {
 	return { props: { company: context.query.company } }
 }
 
 export const Company: React.FC = ({ company }: InferGetServerSidePropsType<typeof getServerSideProps>) => {
-	return <Fragment>Inner Company - {company}</Fragment>
+	return <Fragment>
+		<Container className="mt-5 pt-3">
+			<div>
+				<h1 className="pb-3">{company} Revenue (2015 – 2020)</h1>
+			</div>
+			<div>
+			<InitialTextBox></InitialTextBox>
+			</div>
+			<div>
+			<Bar></Bar>
+			</div>
+		</Container>
+	</Fragment>
 }
 
 export default Company
