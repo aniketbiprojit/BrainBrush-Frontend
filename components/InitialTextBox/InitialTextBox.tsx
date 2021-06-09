a6f62d667b2bc789c050ae953554d67efa89368e
Shamanta Sristy <shamantasristy@gmail.com>
2021-06-09 10:00:48 +0600

    related list structure done

--- components/InitialTextBox/InitialTextBox.tsx
+++ components/InitialTextBox/InitialTextBox.tsx
@@ -1,64 +1,21 @@
 import React from 'react';
-import { makeStyles, Theme, createStyles } from '@material-ui/core/styles';
-import Accordion from '@material-ui/core/Accordion';
-import AccordionDetails from '@material-ui/core/AccordionDetails';
-import AccordionSummary from '@material-ui/core/AccordionSummary';
-import Typography from '@material-ui/core/Typography';
-import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
+import { Card } from 'react-bootstrap';
 
-import { GetServerSideProps, InferGetServerSidePropsType } from 'next'
-import { introdata } from '../RevenueData/RevenueData';
-
-
-const useStyles = makeStyles((theme: Theme) =>
-    createStyles({
-        root: {
-            width: '100%',
-        },
-        heading: {
-            fontSize: theme.typography.pxToRem(15),
-            flexBasis: '33.33%',
-            flexShrink: 0,
-        },
-        secondaryHeading: {
-            fontSize: theme.typography.pxToRem(15),
-            color: theme.palette.text.secondary,
-        },
-    }),
-);
-
-// export const getServerSideProps: GetServerSideProps = async (context) => {
-//     return { props: { company: context.query.company } }
-// }
-const InitialTextBox: React.FC<{}> = (props) => {
-
-    // const { companydata } = props;
-    const classes = useStyles();
-    const [expanded, setExpanded] = React.useState<string | false>(false);
-
-    const handleChange = (panel: string) => (event: React.ChangeEvent<{}>, isExpanded: boolean) => {
-        setExpanded(isExpanded ? panel : false);
-
-        return (
-            <div className={classes.root}>
-                <Accordion expanded={expanded === 'panel1'} onChange={handleChange('panel1')}>
-                    <AccordionSummary
-                        expandIcon={<ExpandMoreIcon />}
-                        aria-controls="panel1bh-content"
-                        id="panel1bh-header"
-                    >
-                        <Typography className={classes.heading}>General settings</Typography>
-                        <Typography className={classes.secondaryHeading}>I am an accordion</Typography>
-                    </AccordionSummary>
-                    <AccordionDetails>
-                        <Typography>
-                            {introdata}
-                        </Typography>
-                    </AccordionDetails>
-                </Accordion>
-            </div>
-        )
-    }
+function InitialTextBox() {
+  return (
+    <>
+      <Card border="light" style={{ width: '18rem' }}>
+        <Card.Header>Header</Card.Header>
+        <Card.Body>
+          <Card.Title>Light Card Title</Card.Title>
+          <Card.Text>
+            Some quick example text to build on the card title and make up the bulk
+            of the card's content.
+      </Card.Text>
+        </Card.Body>
+      </Card>
+    </>
+  );
 }
-    
-    export default InitialTextBox;
+
+export default InitialTextBox;
--- pages/company/[company]/index.tsx
+++ pages/company/[company]/index.tsx
@@ -2,9 +2,11 @@ import React, { Fragment, useState } from 'react'
 
 import { GetServerSideProps, InferGetServerSidePropsType } from 'next'
 import { Button, Container } from 'react-bootstrap'
-import Bar from '../../../components/Charts/Bar'
-import { InitialTextBox } from '../../../components/InitialTextBox/InitialTextBox'
 // import Bar from '../../../components/Charts/Bar'
+// import { InitialTextBox } from '../../../components/InitialTextBox/InitialTextBox'
+// import Bar from '../../../components/Charts/Bar'
+import RelatedListGroupItem from '../../../components/RelatedListGroupItem/RelatedListGroupItem'
+import InitialTextBox from '../../../components/InitialTextBox/InitialTextBox'
 
 export const getServerSideProps: GetServerSideProps = async (context) => {
 	return { props: { company: context.query.company } }
@@ -17,10 +19,13 @@ export const Company: React.FC = ({ company }: InferGetServerSidePropsType<typeo
 				<h1 className="pb-3">{company} Revenue (2015 – 2020)</h1>
 			</div>
 			<div>
-			<InitialTextBox></InitialTextBox>
+				<InitialTextBox></InitialTextBox>
 			</div>
 			<div>
-			<Bar></Bar>
+				{/* <Bar></Bar> */}
+			</div>
+			<div>
+				<RelatedListGroupItem></RelatedListGroupItem>
 			</div>
 		</Container>
 	</Fragment>
